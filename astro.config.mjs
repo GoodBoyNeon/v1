122e2344a57aa1343e573fdaec928f16da732766
GoodBoyNeon <goodboyneon03@gmail.com>
2022-09-02 21:22:50 +0545

    setup github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://goodboyneon.github.io',
 });
 
