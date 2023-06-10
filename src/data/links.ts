78f5d446dcbe72c49976606f4a61adc27ac136b1
GoodBoyNeon <goodboyneon03@gmail.com>
2023-06-10 10:43:46 +0545

    update site

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,7 +1,5 @@
 export const links: { [index: string]: any } = {
-  github: 'https://github.com/GoodBoyNeon',
-  discord: 'https://discord.com/users/816253376962625537',
-  twitter: 'https://twitter.com/goodboyneon',
-}
-
-
+  github: "https://github.com/GoodBoyNeon",
+  discord: "https://discord.com/users/816253376962625537",
+  twitter: "https://twitter.com/neonthenerd",
+};
