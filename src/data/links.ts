a3e66de6aee77000504c651d3f387f55e8842cf2
GoodBoyNeon <goodboyneon03@gmail.com>
2023-06-17 15:25:59 +0545

    update discord username and favicon

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,4 +2,5 @@ export const links: { [index: string]: any } = {
   github: "https://github.com/GoodBoyNeon",
   discord: "https://discord.com/users/816253376962625537",
   twitter: "https://twitter.com/neonthenerd",
+  mail: "mailto:contact@neon.is-a.dev",
 };
