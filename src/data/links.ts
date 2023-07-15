1e1bb4abec418209dc0c71b3921bd32e67b3a7fc
GoodBoyNeon <goodboyneon03@gmail.com>
2023-07-15 08:31:48 +0545

    new redirect link: byte-server

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,6 +1,7 @@
 export const links: { [index: string]: any } = {
-  github: "https://github.com/GoodBoyNeon",
-  discord: "https://discord.com/users/816253376962625537",
-  twitter: "https://twitter.com/neonthenerd",
-  mail: "mailto:contact@neon.is-a.dev",
+  github: 'https://github.com/GoodBoyNeon',
+  discord: 'https://discord.com/users/816253376962625537',
+  twitter: 'https://twitter.com/neonthenerd',
+  mail: 'mailto:contact@neon.is-a.dev',
+  'byte-server': 'https://discord.gg/hQpYpA93uA',
 };
